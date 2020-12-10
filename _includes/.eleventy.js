6f67a23cbd86b2611e4430c8f3d21cd9a055d2b3
Marcus Lindblom <m@marcuslindblom.com>
2020-12-10 11:12:39 +0100

    Initial commit

---
+++ _includes/.eleventy.js
@@ -0,0 +1,8 @@
+module.exports = function (eleventy) {
+  
+  // Add related posts
+  eleventy.addLiquidShortcode("related", (title, url) => `
+  <a href="${url}" class="related">${title}</a>
+  `);
+
+};
